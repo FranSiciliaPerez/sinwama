752104f249b00d994096ade03e15143afdef6b52
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-01-28 14:15:44 +0100

    m

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'sinwama-83cc6',
+    appId: '1:199306172623:web:bb828a6af719ba13733fba',
+    storageBucket: 'sinwama-83cc6.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBZfg5r5jemarejV_xFzWpCbHYg3KKCkFM',
+    authDomain: 'sinwama-83cc6.firebaseapp.com',
+    messagingSenderId: '199306172623',
+  },
   production: false
 };
 
